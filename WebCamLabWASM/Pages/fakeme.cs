401d3ba864e2c56eb7aa0166dd445e2bc2db9e16
HannesPreishuber <hannesp@ppedv.de>
2021-09-17 12:43:56 +0200

    Projektdateien hinzufügen.

---
+++ WebCamLabWASM/Pages/fakeme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using ZXing;
+using ZXing.Common;
+//using ZXing.CoreCompat.System.Drawing;
+namespace WebCamLabWASM.Pages
+{
+    public class fakeme
+    {
+        public fakeme()
+        {
+
+            // var bitmap = new Bitmap();
+            //LuminanceSource source;
+            //source = new ZXing.CoreCompat.System.Drawing.BitmapLuminanceSource(bitmap);
+            //BinaryBitmap b = new BinaryBitmap(new HybridBinarizer(source));
+            //Result result = new MultiFormatReader().decode(b);
+            var stream = new MemoryStream();
+            var barcodeBitmap = new System.DrawingCore.Bitmap(stream);
+            var reader = new ZXing.ZKWeb.BarcodeReader();
+            var result = reader.Decode(barcodeBitmap);
+        }
+    }
+}
---
+++ WebCamLabWASM/wwwroot/js/CamHelper.js
@@ -0,0 +1,49 @@
+﻿const constraints = { audio: false, video: true };
+//https://blog.tomayac.com/2020/05/15/the-requestvideoframecallback-api/
+const updateCanvas = (now, metadata) => {
+  
+   
+    DotNet.invokeMethodAsync('WebCamLabWASM', 'OnFrameArrived', getBase64Img(video));
+    
+    video.requestVideoFrameCallback(updateCanvas);
+};
+var video;
+export async function initialize(v, dotnet) {
+    video = v;
+        let stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream;
+        video.play();
+   
+        
+    video.requestVideoFrameCallback(updateCanvas);
+
+}
+export function Test(video) {
+    console.log("XXXXXXXXXXXXXX");
+    if ('requestVideoFrameCallback' in HTMLVideoElement.prototype) {
+        
+      return true;
+    }
+    return false;
+}
+export function getBase64Img(video) {
+ 
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext('2d');
+    canvas.setAttribute('width', video.videoWidth);
+    canvas.setAttribute('height', video.videoHeight);
+ 
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+   
+    let data = canvas.toDataURL('image/png');
+   
+    canvas.remove();
+    return data;
+}
+
+
+
+function errorMsg(msg, error, dotnet) {
+
+    dotnet.invokeMethodAsync("OnWebCamLiveError", msg);
+}
